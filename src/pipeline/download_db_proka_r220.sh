09b845a439c54928ad2ad39fdd82bd151de3b1b1
Romain Strock <romain.strock@gmail.com>
2024-11-24 11:24:34 +0000

    Parametrise download script

--- src/pipeline/download_db_proka_r220.sh
+++ src/pipeline/download_db_proka_r220.sh
@@ -4,7 +4,19 @@
 #PBS -e error_download_db_proka_r220_qsub.txt
 #PBS -o output_download_db_proka_r220_qsub.txt
 
-DB_PROKA=/rds/general/project/lms-warnecke-raw/live/db_proka_r220
+DB_PROKA_BASE=/rds/general/project/lms-warnecke-raw/live/db_proka_r220
+
+# Input variables can be set with qsub -v
+# If not set the following defaults are used:
+if [ -z "${ACCESSIONS}" ]; then
+    ACCESSIONS="${DB_PROKA_BASE}/db_proka_r220_genus_10s_accessions.txt"
+fi
+if [ -z "${NCBI_SUMMARY}" ]; then
+    NCBI_SUMMARY="${DB_PROKA_BASE}/ncbi_assembly_summary.txt.gz"
+fi
+if [ -z "${OUTPUT_FOLDER}" ]; then
+    OUTPUT_FOLDER=$DB_PROKA_BASE
+fi
 
 cd $HOME
 . load_conda.sh
@@ -12,9 +24,9 @@ cd $HOME
 cd $HOME/assembly-main
 
 python -m src.fetch_assemblies \
-	-l $DB_PROKA/db_proka_r220_genus_10s_accessions.txt \
-	-s $DB_PROKA/ncbi_assembly_summary.txt.gz \
-	-o $DB_PROKA \
+	-l $ACCESSIONS \
+	-s $NCBI_SUMMARY \
+	-o $OUTPUT_FOLDER \
 	--cpu=64 \
 	> $HOME/output_download_db_proka_r220.txt \
 	2> $HOME/error_download_db_proka_r220.txt
