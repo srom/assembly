70580bc687ad396976c5591408da7ff3407f3345
Romain Strock <romain.strock@gmail.com>
2024-11-25 15:16:43 +0000

    TIGR search HPC script

---
+++ src/db_proka/pipeline/search_TIGR_db_proka_r220.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=128:mem=64gb
+#PBS -e error_search_TIGR_db_proka_r220_qsub.txt
+#PBS -o output_search_TIGR_db_proka_r220_qsub.txt
+
+DB_PROKA_BASE=/rds/general/project/lms-warnecke-raw/live/db_proka_r220
+HMM_DB=/rds/general/project/lms-warnecke-raw/live/TIGR_15/TIGR.hmm
+
+# Input variables can be set with qsub -v
+# If not set the following defaults are used:
+if [ -z "${BASE_FOLDER}" ]; then
+    BASE_FOLDER=$DB_PROKA_BASE
+fi
+
+cd $HOME
+. load_conda.sh
+
+cd ${HOME}/assembly-main
+
+python -m src.postprocessing.search_hmm \
+	-i $BASE_FOLDER \
+	-d $HMM_DB \
+    --hmmer_cut_ga \
+	--n_processes 60 \
+	--n_threads_per_process 2 \
+	> ${HOME}/output_search_TIGR_db_proka_r220.txt \
+	2> ${HOME}/error_search_TIGR_db_proka_r220.txt
