ab93eb66c5720f5e1c0eb52c66060f400a3b25d9
Romain Strock <romain.strock@gmail.com>
2024-11-27 17:15:28 +0000

    Linclust script

---
+++ src/db_proka/pipeline/linclust_db_proka_r220.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#PBS -l walltime=72:00:00
+#PBS -l select=1:ncpus=128:mem=128gb
+#PBS -e error_linclust_db_proka_r220_qsub.txt
+#PBS -o output_linclust_db_proka_r220_qsub.txt
+
+set -e
+cd $HOME
+
+DB=/rds/general/project/lms-warnecke-raw/live/db_proka_r220/mmseqs_db/db_proka_r220
+DB_clu=/rds/general/project/lms-warnecke-raw/live/db_proka_r220/mmseqs_db/db_proka_r220_clu90
+DB_clu_rep=/rds/general/project/lms-warnecke-raw/live/db_proka_r220/mmseqs_db/db_proka_r220_clu90_rep
+DB_clu_tsv=/rds/general/project/lms-warnecke-raw/live/db_proka_r220/db_proka_r220_genus_10s_proteins_clu90.tsv
+DB_clu_fasta=/rds/general/project/lms-warnecke-raw/live/db_proka_r220/db_proka_r220_genus_10s_proteins_clu90.fasta
+MIN_SEQ_IDENTITY="0.9"
+
+. load_conda.sh
+conda activate mmseqs
+
+# Run clustering
+mmseqs linclust $DB $DB_clu $TMPDIR \
+    --min-seq-id $MIN_SEQ_IDENTITY \
+    --threads 128 \
+    --split-memory-limit "120G" \
+    > output_linclust_db_proka_r220.txt \
+    2> error_linclust_db_proka_r220.txt
+
+# Output mapping file
+mmseqs createtsv $DB $DB $DB_clu $DB_clu_ts \
+    --threads 128 \
+    >> output_linclust_db_proka_r220.txt \
+    2>> error_linclust_db_proka_r220.txt
+
+# Create representative sub-DB
+mmseqs createsubdb $DB_clu $DB $DB_clu_rep \
+    >> output_linclust_db_proka_r220.txt \
+    2>> error_linclust_db_proka_r220.txt
+
+# Output representative sequences as fasta
+mmseqs convert2fasta $DB_clu_rep $DB_clu_fasta \
+    >> output_linclust_db_proka_r220.txt \
+    2>> error_linclust_db_proka_r220.txt
